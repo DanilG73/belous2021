5d5e129e685c9d3465783e833692b2686e8452e9
DanilG73 <123488618+DanilG73@users.noreply.github.com>
2024-03-31 20:13:24 +0300

    Add files via upload

---
+++ 20 21/ConsoleApp0210/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0210
+{
+    internal class Program
+    {
+        public class Date
+        {
+            public int day; public int month; public int year;
+
+            public Date(int day, int month, int year)
+            {
+                if (year > 0 && month > 0 && day > 0)
+                {
+                    this.day = day;
+                    this.month = month;
+                    this.year = year;
+                }
+            }
+
+            public Date(Date date)
+            {
+                this.day = date.day;
+                this.month = date.month;
+                this.year = date.year;
+            }
+
+            public int Next()
+            {
+                day += 1;
+
+                if (day > 31)
+                {
+                    day = 1;
+                    month += 1;
+                }
+                if (month > 12)
+                {
+                    month = 1;
+                    year += 1;
+                }
+
+                Console.WriteLine($"{day}.{month}.{year}");
+
+                return day;
+            }
+            public int Prev()
+            {
+                day -= 1;
+
+                if (day < 1)
+                {
+                    day = 31;
+                    month -= 1;
+                }
+                if (month < 1)
+                {
+                    month = 12;
+                    year -= 1;
+                }
+
+                Console.WriteLine($"{day}.{month}.{year}");
+
+                return day;
+            }
+
+            public void IsLeapLear()
+            {
+                if (year % 4 == 0)
+                    Console.WriteLine($"{year}-й год является високосным.");
+                else
+                    Console.WriteLine($"{year}-й год не является високосным.");
+            }
+
+            public void Minus(int xDays)
+            {
+                Console.WriteLine($"Предыдущие {xDays} дней: ");
+
+                for (int i = 1; i <= xDays; i++)
+                {
+                    if (day - i < 1)
+                    {
+                        Console.Write($"{day - i + 31}; ");
+                    }
+                    else
+                    {
+                        Console.Write($"{day - i}; ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            public void Plus(int xDays)
+            {
+                Console.WriteLine($"Следующие {xDays} дней: ");
+
+                for (int i = 1; i <= xDays; i++)
+                {
+                    if (day + i > 31)
+                    {
+                        Console.Write($"{day + i - 31}; ");
+                    }
+                    else
+                    {
+                        Console.Write($"{day + i}; ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Date date1 = new(3, 4, 2006);
+
+            date1.Plus(5);
+
+            date1.Minus(5);
+
+            date1.Next();
+            date1.Next();
+            date1.Next();
+
+            date1.Prev();
+            date1.Prev();
+
+            date1.IsLeapLear();
+
+            Date date2 = new(date1);
+
+            date2.Prev();
+
+            Console.ReadLine();
+        }
+    }
+}
+}
---
+++ 20 21/ConsoleApp0220/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0220
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Test
+    {
+        class Question
+        {
+            public string QuestionText { get; set; }
+            public List<string> Options { get; set; }
+            public int CorrectOption { get; set; }
+            public int Points { get; set; }
+        }
+
+        class Answers
+        {
+            public List<int> SelectedOptions { get; set; }
+        }
+
+        private Question question1 = new Question
+        {
+            QuestionText = "What is the capital of France?",
+            Options = new List<string> { "London", "Paris", "Berlin", "Madrid" },
+            CorrectOption = 2,
+            Points = 1
+        };
+
+        private Question question2 = new Question
+        {
+            QuestionText = "Which planet is known as the Red Planet?",
+            Options = new List<string> { "Mars", "Venus", "Jupiter", "Saturn" },
+            CorrectOption = 0,
+            Points = 1
+        };
+
+        private Question question3 = new Question
+        {
+            QuestionText = "What is the largest mammal on Earth?",
+            Options = new List<string> { "Elephant", "Blue Whale", "Giraffe", "Hippopotamus" },
+            CorrectOption = 1,
+            Points = 1
+        };
+
+        private Answers userAnswers = new Answers();
+
+        public void DisplayQuestion(int questionNumber)
+        {
+            Question question = null;
+            switch (questionNumber)
+            {
+                case 1:
+                    question = question1;
+                    break;
+                case 2:
+                    question = question2;
+                    break;
+                case 3:
+                    question = question3;
+                    break;
+                default:
+                    Console.WriteLine("Invalid question number.");
+                    return;
+            }
+
+            Console.WriteLine(question.QuestionText);
+            for (int i = 0; i < question.Options.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {question.Options[i]}");
+            }
+        }
+
+        public void SetUserAnswer(int questionNumber, List<int> selectedOptions)
+        {
+            userAnswers.SelectedOptions = selectedOptions;
+        }
+
+        public void GradeTest()
+        {
+            int correctAnswers = 0;
+            int totalPoints = 0;
+
+            if (question1.CorrectOption == userAnswers.SelectedOptions[0])
+            {
+                correctAnswers++;
+                totalPoints += question1.Points;
+            }
+
+            if (question2.CorrectOption == userAnswers.SelectedOptions[1])
+            {
+                correctAnswers++;
+                totalPoints += question2.Points;
+            }
+
+            if (question3.CorrectOption == userAnswers.SelectedOptions[2])
+            {
+                correctAnswers++;
+                totalPoints += question3.Points;
+            }
+
+            Console.WriteLine($"Number of correct answers: {correctAnswers}");
+            Console.WriteLine($"Number of incorrect answers: {3 - correctAnswers}");
+            Console.WriteLine($"Total points: {totalPoints}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Test test = new Test();
+
+            test.DisplayQuestion(1);
+
+            List<int> userSelectedOptions = new List<int> { 2, 0, 1 };
+
+            test.SetUserAnswer(1, userSelectedOptions);
+
+            test.GradeTest();
+            Console.ReadKey();
+        }
+    }
+    
+}
